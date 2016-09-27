7fcc78a61c4b42efe5448a2a1f5962901e33c8e5
bharadwaj92 <bharadwajt@ymail.com>
2016-09-26 20:08:04 -0500

    Update watsonproblem.py

--- watsonproblem.py
+++ watsonproblem.py
@@ -14,13 +14,16 @@ if(N <= 0 or N >= pow(10,2)):
 data2 = sys.stdin.readline().rstrip()
 temp = data2.split(" ")
 for i in range(len(temp)):
-	if(int(temp[i]) <= 0 or int(temp[i]) >= pow(10,9) ):
+	if(int(temp[i]) <= 0 or int(temp[i]) >= pow(10,9) or len(temp) != N ):
 		print("please enter correct value for array")
 		raise SystemExit
 	else:
 		A.append(int(temp[i]))
 data3 = sys.stdin.readline().rstrip()
 temp1 = data3.split(" ")
+if(len(temp1) != 2):
+	print("enter two values for the range")
+	raise SystemExit
 if(int(temp1[0]) <= 0 or int(temp1[0]) >= pow(10,9) or int(temp1[1]) <= 0 or int(temp1[1]) >= pow(10,9) ):
 	print("please enter correct value for P and Q")
 	raise SystemExit
@@ -36,7 +39,9 @@ else:
 #print(A)
 #print(N , A , P , Q)
 minitemlist = {}
+minMvalue = []
 def minfind():
+	maxvalue = 0
 	for M in PQList:
 		templist = []
 		for ai in A:
@@ -44,6 +49,13 @@ def minfind():
 		minitemlist[M] = min(templist)
 		#print(minitemlist)
 	#print(minitemlist)
-	return(max(minitemlist, key=minitemlist.get))
+	for key in minitemlist.keys():
+		if(minitemlist[key]>= maxvalue):
+			maxvalue = minitemlist[key]
+	for key in minitemlist.keys():
+		if(minitemlist[key] == maxvalue):
+			minMvalue.append(key)
+	minMvalue.sort()
+	return(minMvalue[0])
+			
 print(minfind())
-#print(minitemlist[0])
