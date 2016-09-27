5b80200577a82089f09ccf755fcd57ab8dd80e00
bharadwaj92 <bharadwajt@ymail.com>
2016-09-26 19:21:54 -0500

    Update watsonproblem.py

--- watsonproblem.py
+++ watsonproblem.py
@@ -1,21 +1,3 @@
-##Watson gives Sherlock an array A1,A2...AN. 
-##He asks him to find an integer M between P and Q(both inclusive), such that, min {|Ai-M|, 1 ≤ i ≤ N} is maximised. If there are multiple solutions, print the smallest one. 
-##Input Format 
-##The first line contains N. The next line contains space separated N integers, and denote the array A. The third line contains two space separated integers denoting P and Q. 
-##Output Format 
-##In one line, print the required answer.
-##Constraints 
-##1 ≤ N ≤ 102 
-##1 ≤ Ai ≤ 109 
-##1 ≤ P ≤ Q ≤ 109 
-##Sample Input 
-##3
-##5 8 14
-##4 9
-##Sample Output   6 min[1 2 8]
-##4
-##Explanation 
-##For M = 4,6,7, or 9, the result is 1. Since we have to output the smallest of the multiple solutions, we print 4.
 import sys
 import math
 N = 0
